1e438e86965d04c8963e456b003abbcdb98e692d
vagrant <vagrant@localhost.localdomain>
2020-08-04 20:44:03 +0900

    fix

--- memo.js
+++ memo.js
@@ -125,3 +125,12 @@ $(function(){
 });
 
 children指定
+$(function(){
+    $('button').on('click', function(){
+        $('ul').children().css('color', 'red');
+    });
+
+    $('button').mouseout(function(){
+        $('ul').children().css('color', 'blue');
+    });
+});
